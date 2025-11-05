4d838707091001dff775f56c985ae919b2d9bce0
Pavel Zhukov <cray0000@gmail.com>
2025-11-05 18:37:33 +0200

    fix: export matcher, variables, dimensions from @cssxjs/runtime and from the main cssxjs

--- packages/cssxjs/index.js
+++ packages/cssxjs/index.js
@@ -1,3 +1,8 @@
+export { default as variables } from '@cssxjs/runtime/variables'
+export { defaultVariables, setDefaultVariables } from '@cssxjs/runtime/variables'
+export { default as dimensions } from '@cssxjs/runtime/dimensions'
+export { default as matcher } from '@cssxjs/runtime/matcher'
+
 export function css (cssString) {
   throw Error('[cssxjs] Unprocessed \'css\' template string. Bundler (Babel / Metro) did not process this file correctly.')
 }
